8043a7a85361587f77c893ea404fa762f083404d
Logon Aluno <logonrmlocal@fiap.com.br>
2018-09-27 19:31:41 -0300

    Initial commit

---
+++ app/src/main/java/br/com/bicmsystems/pokemonapp/PesquisaActivity.kt
@@ -0,0 +1,12 @@
+package br.com.bicmsystems.pokemonapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PesquisaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pesquisa)
+    }
+}
